5532b77a35dcf36a3ee3715de9f34406ed4d5c9a
Aleksey <alexdudakov@gmail.com>
2020-09-17 18:49:11 +0500

    Create wrapper function for check_disk_full

--- check-reboot.py
+++ check-reboot.py
@@ -1,3 +1,6 @@
 """ This is check reboot file """
 """ Add some feature """
+def check_root_full():
+	return check_disk_full("disk="/", min_gb=2, min_percent=10)
+
 """ Some corrections to he code"""
